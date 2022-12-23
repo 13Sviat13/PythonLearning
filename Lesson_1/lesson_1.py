803f795deb02cca81bbce61232e2cf7b8dfb4b77
13.sviat github.io <13.sviat@gmail.com>
2022-12-23 17:12:52 +0200

    git@github.com:13Sviat13/PythonLearning.git

---
+++ Lesson_1/lesson_1.py
@@ -0,0 +1,16 @@
+#Task1
+print("What's your name?  " )
+name = input() + " "
+print("Well..." + name.title() + "..." + "and.. what's your surname?")
+surname = input()
+full_name = name + surname
+print("Heh I see " + full_name.title() +  " very interesting")
+
+rep_of_names = name*5
+print("Okay " + name + "your name..ha-ha some strange\nNo...no I'm not laughing but " + rep_of_names.upper() + "sorry.")
+
+print("Okay, just try again")
+name2 = name.replace(" ", "\n")
+print("Well " + name2 + "Nice to meet you")
+
+#task2
