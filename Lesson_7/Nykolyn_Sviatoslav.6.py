6631ef2a61a385426a2a25bcb6ffbbe84db5a223
13.sviat github.io <13.sviat@gmail.com>
2023-01-25 22:28:37 +0200

    Homework 6.1

--- Lesson_7/Nykolyn_Sviatoslav.6.py
+++ Lesson_7/Nykolyn_Sviatoslav.6.py
@@ -1,50 +1,93 @@
+import numpy as np
+
+def menu():
+    print("Choose the task(write from 1 to 5)")
+    print("Task_1. Output file without .dot\n"
+          "Task_2. Output only surname from file\n"
+          "Task_3. Output only data from file\n"
+          "Task_4. Sort string by length and overwrites it from file")
+    answer = input()
+    if answer == '1':
+        open_without()
+    elif answer == '2':
+        surname_journal()
+    elif answer == '3':
+        read_from_txt()
+    elif answer == '4':
+        sort_by_length()
+    elif answer == '5':
+        exit()
+
 
 # task_1
-# def open_without():
-#     # this function output text from file in one line and without '.'
-#     with open('domains.txt', 'r') as file:
-#         for line in file:
-#             read = file.read()
-#             replace = read.replace('.', '')
-#             lines = replace.splitlines()
-#             print(lines)
-#
-#
-# open_without()
+def open_without():
+    # this function output text from file in one line and without '.'
+    with open('domains.txt', 'r') as file:
+        for line in file:
+            read = file.read()
+            replace = read.replace('.', '')
+            lines = replace.splitlines()
+            print(lines)
 
-# import numpy as np
+
+
+
+
 # # task_2
-# def surname_journal():
-#     names  = "17. Malynovskyi  France\n" \
-#              "15. Mudryk       England\n"\
-#              "3.  Sydorchuk    Ukraine\n"\
-#              "14. Zinchenko    England\n"\
-#              "13. Lunin        Spain\n"\
-#              "9.  Yaremchuk    Belgium\n"\
-#              "10. Tsigankov    Spain\n"\
-#              "7.  Yarmolenko   UAA"
-#
-#     journal = open('names.txt', 'w')
-#
-#     str = journal.write(names)
-#     arr = names.split()
-#     print(arr)
-#     our_array = np.array(arr)
-#     numpy = np.array_split(our_array, 24)
-#     chunked_list = [list(array) for array in numpy]
-#     print(chunked_list[1::3])
-#
-# surname_journal()
+def surname_journal():
+    names = "17. Malynovskyi  France\n" \
+             "15. Mudryk       England\n"\
+             "3.  Sydorchuk    Ukraine\n"\
+             "14. Zinchenko    England\n"\
+             "13. Lunin        Spain\n"\
+             "9.  Yaremchuk    Belgium\n"\
+             "10. Tsigankov    Spain\n"\
+             "7.  Yarmolenko   UAA"
+
+    journal = open('names.txt', 'w')
+
+    str = journal.write(names)
+    arr = names.split()
+    print(arr)
+    our_array = np.array(arr)
+    numpy = np.array_split(our_array, 24)
+    chunked_list = [list(array) for array in numpy]
+    print(chunked_list[1::3])
+
+
 
 # task_3
 def read_from_txt():
-    my_dict = {}
-    with open('authors.txt') as fileobj:
-        for line in fileobj:
-            key, value = line.split(":")
-            my_dict[key] = value
-
+    opening = open('authors.txt', 'r')
+    reading = opening.read()
+    read = reading.split("\n")
+    d = []
+    for lines in read:
+        date = lines.split("-")
+        if date[0] and not date[0][0].isalpha():
+            d.append(({"date": date[0]}))
+        else:
+            continue
     print(d)
 
-read_from_txt()
 
+
+def sort_by_length():
+    new = "big better stongest\n" \
+          "master mini fool\n" \
+          "ki-ki karol binial"
+    file = open("file with symbols.txt", "w")
+    file_write = file.write(new)
+    file.close()
+    file1 = open("file with symbols.txt", "r")
+    line = file1.readlines()
+    print(line)
+    sort1 = new.split()
+    line1 = sorted(sort1, key=len)
+    print(line1)
+    file1.close()
+    f
+
+
+
+menu()
