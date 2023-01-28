219f6674dfebc3fbe6096b2680548d1850a85f8b
13.sviat github.io <13.sviat@gmail.com>
2023-01-28 06:16:19 +0200

    Homework 7(demo)

--- Lesson_8/Nykolyn_Sviatoslav_7.py
+++ Lesson_8/Nykolyn_Sviatoslav_7.py
@@ -7,59 +7,59 @@ import numpy
 from dateutil import parser
 import pandas as pd
 
-# task_1
-# data = [{
-#     "Name": "Sviatoslav",
-#     "Surname": "Nykolyn",
-#     "Category": "Python Basic",
-#     "Rating": 2
-#     },
-#     {
-#         "Name": "Oleksandr",
-#         "Surname": "Osadchyi",
-#         "Category": "Python basic",
-#         "Rating": 1,
-#     },
-#     {
-#         "Name": "Mykyta",
-#         "Surname": "Klochko",
-#         "Category": "Python Basic",
-#         "Rating": 3,
-#     }]
-#
-# def data_json():
-#     with open("task_1.json", "w+") as file:
-#         json.dump(data, file, indent=4)
-#         json_str = json.dumps(data, indent=4)
-#         print(json_str)
+task_1
+data = [{
+    "Name": "Sviatoslav",
+    "Surname": "Nykolyn",
+    "Category": "Python Basic",
+    "Rating": 2
+    },
+    {
+        "Name": "Oleksandr",
+        "Surname": "Osadchyi",
+        "Category": "Python basic",
+        "Rating": 1,
+    },
+    {
+        "Name": "Mykyta",
+        "Surname": "Klochko",
+        "Category": "Python Basic",
+        "Rating": 3,
+    }]
+
+def data_json():
+    with open("task_1.json", "w+") as file:
+        json.dump(data, file, indent=4)
+        json_str = json.dumps(data, indent=4)
+        print(json_str)
 
 
 # data_json()
 
 # task_2
-# def parse_csv():
-#
-#     data2 = [["Firstname", "Lastname", "Birthdate", "Marks", "Comments"],
-#              ['Ada', 'Lovelace', parser.parse('1985/12/10').date(), [4242,1010], 'The first one'],
-#              ["Linus", "Torvald", parser.parse('12/28/1969').date(), [42,21], "Have a problem with penguin"],
-#              ["Theo", "De Raadt", parser.parse('05/19/1968').date(), [18,19,20], "This guy is just crazy"],
-#              ["Dennis", "Ritchie", parser.parse('09/09/1941').date(), [20,20,20], "Like a boss"],
-#              ["Alan", "Turing", parser.parse('06/23/1912').date(), [42,42,42], "Shouldn't eat apple"]]
-#
-#
-#
-#     with open("task2.csv", "w+") as file:
-#         writer = csv.writer(file)
-#         for row in data2:
-#             writer.writerow(row)
-#
-#     with open("task2.csv", "r") as f:
-#         dict_reader = csv.DictReader(f)
-#         list_of_dict = list(dict_reader)
-#         print(json.dumps(list_of_dict, indent=5))
-#
-#
-# parse_csv()
+def parse_csv():
+
+    data2 = [["Firstname", "Lastname", "Birthdate", "Marks", "Comments"],
+             ['Ada', 'Lovelace', parser.parse('1985/12/10').date(), [4242,1010], 'The first one'],
+             ["Linus", "Torvald", parser.parse('12/28/1969').date(), [42,21], "Have a problem with penguin"],
+             ["Theo", "De Raadt", parser.parse('05/19/1968').date(), [18,19,20], "This guy is just crazy"],
+             ["Dennis", "Ritchie", parser.parse('09/09/1941').date(), [20,20,20], "Like a boss"],
+             ["Alan", "Turing", parser.parse('06/23/1912').date(), [42,42,42], "Shouldn't eat apple"]]
+
+
+
+    with open("task2.csv", "w+") as file:
+        writer = csv.writer(file)
+        for row in data2:
+            writer.writerow(row)
+
+    with open("task2.csv", "r") as f:
+        dict_reader = csv.DictReader(f)
+        list_of_dict = list(dict_reader)
+        print(json.dumps(list_of_dict, indent=5))
+
+
+parse_csv()
 
 #task_3
 def Convert(a):
