55e4d8c17d27422e9c059e27f960d8b1bcb9044b
13.sviat github.io <13.sviat@gmail.com>
2023-01-29 23:14:09 +0200

    Homework 7

--- Lesson_8/Nykolyn_Sviatoslav_7.py
+++ Lesson_8/Nykolyn_Sviatoslav_7.py
@@ -1,40 +1,84 @@
 import datetime
 import json
 import csv
-from  collections import defaultdict
-
-import numpy
 from dateutil import parser
 import pandas as pd
 
-task_1
+
+def menu():
+    print("It's 3 task")
+    print("(1)Task_1. Dict in Json file\n"
+          "(2)Task_2. Dict from csv file\n"
+          "(3)Task_3. Nasted Tuples in csv file\n"
+          "(4)Exit")
+    answer = input("Write what task you want: ")
+    if answer == "1":
+        data_json()
+    elif answer == "2":
+        parse_csv()
+    elif answer == "3":
+        task_3()
+    elif answer == "4":
+        exit()
+    else:
+        print("I don't understant, what you want...Okay let's try again")
+        return menu()
+
+def back():
+    print("if you want to come back to menu enter 'back'. If you want to close the program write 'exit'")
+    back = str(input().title())
+    if back == 'Back':
+        # this condition means return when typing "back" or close the program when typing "exit"
+        return menu()
+    elif back == 'Exit':
+        exit()
+
+# task_1
 data = [{
     "Name": "Sviatoslav",
     "Surname": "Nykolyn",
+    "Age": 18,
+    "Study": "LNU",
+    "Hobby": "History",
+    "Book": "1984",
+    "Film": "Back to future",
+    "Married": "No",
     "Category": "Python Basic",
-    "Rating": 2
+    "Rating": 1
     },
     {
-        "Name": "Oleksandr",
-        "Surname": "Osadchyi",
-        "Category": "Python basic",
-        "Rating": 1,
+        "Name": "Roman",
+        "Surname": "Molchan",
+        "Age": 18,
+        "Study": "Politech",
+        "Hobby": "football",
+        "Book": "don't read",
+        "Film": "dont't have",
+        "Married": "No",
+        "Category": "C++",
+        "Rating": 2
     },
     {
-        "Name": "Mykyta",
-        "Surname": "Klochko",
+        "Name": "Mykola",
+        "Surname": "Mykhailuk",
+        "Age": 19,
+        "Study": "don't have",
+        "Hobby": "Music",
+        "Book": "Harry Potter",
+        "Film": "Harry Potter",
+        "Married": "Yes",
         "Category": "Python Basic",
-        "Rating": 3,
+        "Rating": 4
     }]
 
 def data_json():
     with open("task_1.json", "w+") as file:
-        json.dump(data, file, indent=4)
-        json_str = json.dumps(data, indent=4)
+        json.dump(data, file, indent=10)
+        json_str = json.dumps(data, indent=10)
         print(json_str)
+        back()
 
 
-# data_json()
 
 # task_2
 def parse_csv():
@@ -57,38 +101,38 @@ def parse_csv():
         dict_reader = csv.DictReader(f)
         list_of_dict = list(dict_reader)
         print(json.dumps(list_of_dict, indent=5))
+    back()
 
 
-parse_csv()
 
 #task_3
-def Convert(a):
-    it = iter(a)
-    res_dct = dict(zip(it, it))
-    return res_dct
-
-
-meteo = [(('temperature1', 42),
-          ('date1', datetime.date(2017, 1, 22)),
-          ('locations1', ('Berlin', 'Paris')),
-          ('weather1', 'sunny')),
+meteo = [(('temperature', 42),
+          ('date', datetime.date(2017, 1, 22)),
+          ('locations', ('Berlin', 'Paris')),
+          ('weather', 'sunny')),
          (('temperature', -42),
           ('date', datetime.date(2017, 1, 22)),
           ('locations', ('Marseille', 'Moscow')),
           ('weather', 'cloudy'))]
-arr = numpy.asarray(meteo, dtype=object)
-
-fla_arr = arr.flatten()
-print(fla_arr)
-my_dict = Convert(fla_arr)
-print(my_dict)
 
 
-def turple():
-    df = pd.DataFrame(my_dict)
-    df.to_csv('task_3.csv', index=False, header=True)
-    # with open('task_3.csv.csv', 'w', newline='') as f:
-    #     writer = csv.writer(f)
-    #     writer.writerows(meteo)
 
-turple()
+
+
+def task_3():
+    my_dict = list(map(dict, meteo))
+    print(my_dict)
+
+    with open("task_3.csv", "w+") as file:
+        datf = pd.DataFrame.from_dict(my_dict)
+        datf.to_csv(r'task_3.csv', index=False, header=True)
+
+
+    with open("task_3.csv", "r") as f:
+            dict_reader = csv.DictReader(f)
+            list_of_dict = list(dict_reader)
+            print(json.dumps(list_of_dict, indent=4))
+    back()
+
+
+menu()
